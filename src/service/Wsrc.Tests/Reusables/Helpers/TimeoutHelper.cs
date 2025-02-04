d599942333845eaed45d23a2c6e085afc3ba8278
siuramka <marius.kamarauskas9999@gmail.com>
2025-02-04 20:11:34 +0200

    increate timeout interval

--- src/service/Wsrc.Tests/Reusables/Helpers/TimeoutHelper.cs
+++ src/service/Wsrc.Tests/Reusables/Helpers/TimeoutHelper.cs
@@ -3,7 +3,7 @@ namespace Wsrc.Tests.Reusables.Helpers;
 public static class TimeoutHelper
 {
     private static readonly TimeSpan PollInterval = TimeSpan.FromMilliseconds(250);
-    private static readonly TimeSpan DefaultTimeout = TimeSpan.FromSeconds(30);
+    private static readonly TimeSpan DefaultTimeout = TimeSpan.FromSeconds(600);
 
     public static async Task WaitUntilAsync(Func<Task<bool>> conditionTask)
     {
